c1ffa8ce73ab5aec4de94325f37c7195e341763b
Jeremy Nauta <Jeremypnauta@gmail.com>
2017-11-28 09:52:58 -0700

    chore(*) | update comments

--- 9.6/10-replication-config.sh
+++ 9.6/10-replication-config.sh
@@ -5,13 +5,12 @@ echo [*] configuring $REPLICATION_ROLE instance
 
 echo "max_connections = $PG_MAX_CONNECTIONS" >> "$PGDATA/postgresql.conf"
 
-# We set master replication-related parameters for both slave and master,
-# so that the slave might work as a primary after failover.
+# Slave and master settings
 echo "wal_level = hot_standby" >> "$PGDATA/postgresql.conf"
 echo "wal_keep_segments = $PG_WAL_KEEP_SEGMENTS" >> "$PGDATA/postgresql.conf"
 echo "max_wal_senders = $PG_MAX_WAL_SENDERS" >> "$PGDATA/postgresql.conf"
 
-# slave settings
+# Slave settings
 if [ $REPLICATION_ROLE = "slave" ]; then
     echo "hot_standby = on" >> "$PGDATA/postgresql.conf"
     echo "primary_conninfo = postgresql://$POSTGRES_MASTER_SERVICE_USER:$POSTGRES_MASTER_SERVICE_PASSWORD@$POSTGRES_MASTER_SERVICE_HOST:POSTGRES_MASTER_SERVICE_PORT" >> "$PGDATA/postgresql.conf"
