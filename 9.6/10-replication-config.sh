a98d844c9c1d37cad35bc0b3c8da0da231d55a4b
Jeremy Nauta <Jeremypnauta@gmail.com>
2017-11-28 09:49:47 -0700

    feat(*) | set `primary_conninfo` to automatically sync slave to master

--- 9.6/10-replication-config.sh
+++ 9.6/10-replication-config.sh
@@ -11,8 +11,10 @@ echo "wal_level = hot_standby" >> "$PGDATA/postgresql.conf"
 echo "wal_keep_segments = $PG_WAL_KEEP_SEGMENTS" >> "$PGDATA/postgresql.conf"
 echo "max_wal_senders = $PG_MAX_WAL_SENDERS" >> "$PGDATA/postgresql.conf"
 
-# slave settings, ignored on master
-echo "hot_standby = on" >> "$PGDATA/postgresql.conf"
-echo "primary_conninfo = postgresql://$POSTGRES_MASTER_SERVICE_USER:$POSTGRES_MASTER_SERVICE_PASSWORD@$POSTGRES_MASTER_SERVICE_HOST:POSTGRES_MASTER_SERVICE_PORT" >> "$PGDATA/postgresql.conf"
+# slave settings
+if [ $REPLICATION_ROLE = "slave" ]; then
+    echo "hot_standby = on" >> "$PGDATA/postgresql.conf"
+    echo "primary_conninfo = postgresql://$POSTGRES_MASTER_SERVICE_USER:$POSTGRES_MASTER_SERVICE_PASSWORD@$POSTGRES_MASTER_SERVICE_HOST:POSTGRES_MASTER_SERVICE_PORT" >> "$PGDATA/postgresql.conf"
+fi
 
 echo "host replication $REPLICATION_USER 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
